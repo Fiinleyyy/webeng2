f5a8cafa7d723e00ed9967173380414de61190ab
tobiassng <148528666+tobiassng@users.noreply.github.com>
2025-06-20 09:26:36 +0000

    added inquirer to either use http or https

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,26 @@ import path from 'path';
 import fs from 'fs';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
 const BUILD_DIR = path.resolve(__dirname, './www');
 
-
 export default {
-  plugins: [
-    react(),
-    mkcert()
-  ],
+  plugins: [react(), mkcert()],
   root: SRC_DIR,
-  base: './', // Relativer Pfad notwendig für SW und PWA
+  base: './',
   publicDir: PUBLIC_DIR,
   build: {
     outDir: BUILD_DIR,
     assetsInlineLimit: 0,
     emptyOutDir: true,
     rollupOptions: {
-      treeshake: true, // für Produktion empfohlen
+      treeshake: true,
     },
   },
   resolve: {
@@ -34,6 +33,6 @@ export default {
     https: true,
     host: true,
     port: 5173,
-    open: false
+    open: false,
   },
 };
